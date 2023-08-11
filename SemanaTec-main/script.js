dcacfa527522b52c11822524fae4c798984900dd
mel <125655569+meinyzx@users.noreply.github.com>
2023-08-11 08:35:44 -0300

    Add files via upload

--- SemanaTec-main/script.js
+++ SemanaTec-main/script.js
@@ -1,38 +1,45 @@
-const pessoa = document.querySelector('.pessoa');
-const obstaculo = document.querySelector('.obstaculo');
+let pessoa = document.querySelector('.pessoa');
+let obstaculo = document.querySelector('.obstaculo');
 
-const jump = () => {
-  pessoa.classList.add('jump');
+function remove(){
+  pessoa.classList.remove('jump');
 
-  setTimeout(()=> {
-               pessoa.classList.remove('jump')
-
-
-  }, 500);
 }
 
-const loop = setInterval(() =>{
 
-        const obstaculoPosition = obstaculo.offsetLeft;
-        const pessoaPosition = window.getComputedStyle(pessoa).bottom.replace('px');
 
-        console.log(pessoaPosition)
-        console.log(obstaculoPosition)
+function jump () {
+  pessoa.classList.add('jump');
 
-      if(obstaculoPosition < 120 && obstaculoPosition > 0 && pessoaPosition < 80)  { 
+setTimeout(remove,500);
+
+ 
+}
+
+
+
+
+function loopgame(){
+
+        let obstaculoPosition = obstaculo.offsetLeft;
+      let pessoaPosition = +window.getComputedStyle(pessoa).bottom.replace('px','');
+      
+
+      if(obstaculoPosition <= 120 && obstaculoPosition > 0 && pessoaPosition < 80)  { 
 
         obstaculo.style.animation = 'none';
-        obstaculo.style.left = '${obstaculoPosition} px';
+        obstaculo.style.left = '${obstaculoPosition} px' ;
 
-        pessoa.style.animation = 'none';
-        pessoa.style.left = '${pessoaPosition} px';
+       
 
-        pessoa.src='pessoa.gameover.jpeg';
+        pessoa.src='./imagens/pessoa.gameover.png';
         pessoa.style.width= '75px';
 
 
       }
 
-},10);
+}
+
+let loop= setInterval(loopgame,10);
 
 document .addEventListener('keydown', jump) 
