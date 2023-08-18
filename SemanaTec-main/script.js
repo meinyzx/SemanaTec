b19efb0093a8f0d4d8fcfb3587f83e48024b99ad
mel <125655569+meinyzx@users.noreply.github.com>
2023-08-18 11:52:40 -0300

    Add files via upload

--- SemanaTec-main/script.js
+++ SemanaTec-main/script.js
@@ -1,47 +1,40 @@
-const pessoa = document.querySelector('.pessoa');
-const obstaculo = document.querySelector('.obstaculo');
+const peesoa = document.querySelector(".pessoa");
+const obstaculo = document.querySelector(".obstaculo");
+const score = document.querySelector(".score");
+let alreadyJump = false;
+let count = 0;
 
+document.addEventListener("keydown", (e) => {
+  if ((e.code === "ArrowUp") | (e.code === "Space")) {
+    jump();
+  }
+});
 
+function jump() {
+  if (!peesoa.classList.contains("jump")) {
+    pessoa.classList.add("jump");
+    alreadyJump = true;
 
-const jump = () => {
-  pessoa.classList.add(jump);
-
-setTimeout(() =>{
-
-const remove
-pessoa.classList.remove(jump);
-},500);
-
-
+    setTimeout(() => {
+     pessoaclassList.remove("jump");
+      alreadyJump = false;
+    }, 1100);
+  }
 }
 
+setInterval(() => {
+  let pessoaBottom = parseInt(
+    window.getComputedStyle(pessoa).getPropertyValue("bottom")
+  );
+  let obstaculoLeft = parseInt(
+    window.getComputedStyle(obstaculo).getPropertyValue("left")
+  );
 
+  if (obstaculoLeft > 40 && obstaculoLeft < 270 && pessoaBottom <= 50 && !alreadyJump) {
+    alert(`Game Over! Seu score foi: ${count}`);
+    count = 0;
+  }
 
-
-const loopgame = setInterval (()=> {
-
-       const obstaculoPosition = obstaculo.offsetLeft;
-      const pessoaPosition = +window.getComputedStyle(pessoa).bottom.replace('px','');
-
-      console.log (obstaculoPosition )
-      console.log( pessoaPosition)
-
-      if(obstaculoPosition <= 120 && obstaculoPosition > 0 && pessoaPosition < 80)  { 
-
-        obstaculo.style.animation = "none";
-        obstaculo.style.left = '${obstaculoPosition}px' ;
-
-        pessoa.style.animation = "none";
-        pessoa.style.left = '${pessoaPosition}px' ;
-
-
-        pessoa.src='./imagens/pessoa.gameover.png';
-        pessoa.style.width= "75px";
-        pessoa.style.marginleft="45px";
-
-        clearInterval(loopgame)
-
-      }
-},5)
-
-document .addEventListener('keydown', jump) 
+  count++;
+  score.innerHTML = `SCORE: ${count}`;
+}, 10);
