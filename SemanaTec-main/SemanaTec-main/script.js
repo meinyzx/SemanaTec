67aad53e4b1a0ecae6851f85b40887ab38a0d99a
mel <125655569+meinyzx@users.noreply.github.com>
2023-08-18 08:41:34 -0300

    Add files via upload

--- SemanaTec-main/SemanaTec-main/script.js
+++ SemanaTec-main/SemanaTec-main/script.js
@@ -1,42 +1,55 @@
-const pessoa = document.querySelector('.pessoa');
-const obstaculo = document.querySelector('.obstaculo');
-const score = document.querySelector('.score');
-let alreadyjump = false;
+const pessoa = document.querySelector(".pessoa");
+const obstaculo = document.querySelector(".obstaculo");
+const score = document.querySelector(".score")
+let alreadyJump = false;
 let count = 0;
 
-document.addEventListener("keydown", (e) =>{
-  if((e.code === "ArrowUp") | (e.code === "Space")) {
-    jump();}
-});
+documento.addEventListener("keydown", (e) => {
+  if ((e.code === "ArrowdUp") ( e.code === "Space")){
+    jump();
+  }
+} );
 
 function jump () {
-  if(!pessoa.classList.contains("jump")){
-    pesssoa.classList.add("jump");
-    alreadyjump = true;
+if(!pessoa.classList.contains(jump)){
+  pessoa.classList.add (jump);
+   alreadyJump=true;
 
-    setTimeout(()=>{
-      pessoa.classList.remove("jump");
-      alreadyjump=false;  
-    },1100)
+  setTimeout(() =>{
+    pessoa.classList.remove(jump);
+    alreadyJump = false
+  },1100);
+  
   }
-}
+}  
 
-setInterval (() => {
 
-  let pessoabottom = parseInt(
-    window.getComputedStyle(pessoa).getPropertyVallue("bottom")
-  ); 
-   let obstaculoleft = parseInt(
-    window.getComputedStyle(obstaculo).getPropertyVallue("left")
-   );
 
-   if (obstaculoleft > 40 && obstaculoleft < 270 && pessoabottom <= 50 && !alreadyjump) {
-    alert ('game over! seu score foi: ${count}'); count=0;
-   }
-   count++; score:innerHTML = 'SCORE:${count}';
+
+const loopgame = setInterval (()=> {
+
+       const obstaculoPosition = obstaculo.offsetLeft;
+      const pessoaPosition = +window.getComputedStyle(pessoa).bottom.replace('px','');
+
+      console.log (obstaculoPosition )
+      console.log( pessoaPosition)
+
+      if(obstaculoPosition <= 120 && obstaculoPosition > 0 && pessoaPosition < 80)  { 
+
+        obstaculo.style.animation = "none";
+        obstaculo.style.left = '${obstaculoPosition}px' ;
+
+        pessoa.style.animation = "none";
+        pessoa.style.left = '${pessoaPosition}px' ;
+
+
+        pessoa.src='./imagens/pessoa.gameover.png';
+        pessoa.style.width= "75px";
+        pessoa.style.marginleft="45px";
+
+        clearInterval(loopgame)
+
+      }
 },10)
 
-
-
-
-
+document .addEventListener('keydown', jump) 
